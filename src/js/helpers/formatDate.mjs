89ca59f082f5744388033cc76772b30ddc214ce5
lhvk <77023253+lhvk@users.noreply.github.com>
2023-03-02 22:30:19 +0100

    refactored and cleaned up some code. Changed some structure

---
+++ src/js/helpers/formatDate.mjs
@@ -0,0 +1,11 @@
+export function formatDate(date, locale) {
+  const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), new Date(date));
+
+  if (daysPassed === 0) return "today";
+  if (daysPassed === 1) return "yesterday";
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  return new Intl.DateTimeFormat(locale).format(date);
+}
--- src/js/templates/listings.mjs
+++ src/js/templates/listings.mjs
@@ -1,42 +1,40 @@
-import { formatDate } from "../controllers/formatDate.mjs";
+import { formatDate } from "../helpers/index.mjs";
 
 // MULTIPLE LISTINGS
 
 export function listingsTemplate(listingData) {
   return ` 
-    <a href="/listing.html?id=${
-      listingData.id
-    }" class="card p-5 mb-5 border-0 listings-card">
-    <div id="listingsMediaContainer" class="overflow-hidden">
+  <a href="/listing.html?id=${listingData.id}" class="card p-5 mb-5 border-0 listings-card">
+  <div id="listingsMediaContainer" class="overflow-hidden">
     <img
-    src="${listingData.media}"
-    onerror="this.onerror=null; 
-        this.src='/src/assets/rubiks.png'"
-    class="listings-card-image"
-    alt="Image of the listing for sale" />
+      src="${listingData.media}"
+      onerror="this.onerror=null; 
+      this.src='/src/assets/rubiks.png'"
+      class="listings-card-image"
+      alt="Image of the listing for sale" />
+  </div>
+
+  <div class="card-body p-0 mt-3 d-flex flex-column justify-content-between">
+ 
+      <div>
+        <h3 class="card-title text-capitalize">${listingData.title}</h3>
+        <p class="mb-2 text-secondary fw-bold">${listingData.tags}</p>
+      </div>
+
+   
+      <div class="d-grid gap-2">
+      <div class="mb-2">
+      <p class="card-text m-0">Created</p>
+      <p class="card-text fw-bold">${formatDate(new Date(listingData.created))}</p>
+    </div>
+      <button href="/listing.html?id=${listingData.id}" class="btn btn-outline-light">view</button>
     </div>
   
-    <div class="card-body p-0 mt-3">
-      <h3 class="card-title mb-5 text-capitalize">${listingData.title}</h3>
-      <p class="card-text m-0">Created</p>
-      <p class="card-text fw-bold">${formatDate(
-        new Date(listingData.created)
-      )}</p>
-      
-  <div class="mb-2">${listingData.tags}</div>
-        
-      <div class="d-grid gap-2">
-        <button href="/listing.html?id=${
-          listingData.id
-        }" class="btn btn-outline-light">view</button>
-      </div>
-    </div> </a
-  >`;
+  </div>
+</a>
+`;
 }
 
 export function renderListingsTemplate(listings, parent) {
-  parent.insertAdjacentHTML(
-    "beforeend",
-    listings.map(listingsTemplate).join("")
-  );
+  parent.insertAdjacentHTML("beforeend", listings.map(listingsTemplate).join(""));
 }
